1929faea986b2f4cdac69941102dc9a1a4ee5e4d
unknown <a.ambia9000@gmail.com>
2022-01-01 23:36:08 -0800

    update formatting

---
+++ 20. HTTP-JSON-AJAX/notes.js
@@ -0,0 +1,16 @@
+// ********************************************************************************************************************************
+// ----------------------------------- 243 HTTP / HTTPS
+// ********************************************************************************************************************************
+
+// 4 methods
+// GET - get an html file from a server
+// POST - send the data to a server and have it added to the server/ database
+// PUT - Update data on a server with new information
+// DELETE - Delete a piece of data on the server
+
+
+
+// a GET request will request the data using a query string in the url 
+// ex.     bob.com/slash?firstname=&lastName=&address=
+
+// a POST request will send the the data using ur query placeholders in html form
